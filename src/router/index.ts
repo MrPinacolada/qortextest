ac778451847a7507bd01ab7f83bf56200fa25b53
MrPinacolada <vasiasko112@gmail.com>
2023-08-16 11:47:01 +0800

    add routing

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter,createWebHashHistory } from "vue-router";
 import Singers from "@/views/Singers.vue";
 import Albums from "@/views/Albums.vue";
 import Addsong from "@/views/Addsong.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
